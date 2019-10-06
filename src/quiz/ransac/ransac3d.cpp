aed0c0380165870e285e3a5920cfcb4f11f1725b
jin <Jin Zhu>
2019-10-06 16:21:59 -0700

    forloop ransac3d

--- src/quiz/ransac/ransac3d.cpp
+++ src/quiz/ransac/ransac3d.cpp
@@ -18,7 +18,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D()
 
 pcl::visualization::PCLVisualizer::Ptr initScene()
 {
-	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
+	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("3D Viewer"));
 	viewer->setBackgroundColor (0, 0, 0);
   	viewer->initCameraParameters();
   	viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
@@ -42,24 +42,17 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 			inliers.insert(rand()%(cloud->points.size()));
 		}
 		
-		float x1, y1, z1, x2, y2, z2, x3, y3, z3;
-		auto itr = inliers.begin();
-		x1 = cloud->points[*itr].x;
-		y1 = cloud->points[*itr].y;
-        z1 = cloud->points[*itr].z; 
-		itr++;
-		x2 = cloud->points[*itr].x;
-		y2 = cloud->points[*itr].y;
-        z2 = cloud->points[*itr].z;
-        itr++;
-        x3 = cloud->points[*itr].x;
-		y3 = cloud->points[*itr].y;
-        z3 = cloud->points[*itr].z;
+		std::vector<float> x, y, z;
+        for(auto itr = inliers.begin(); itr!=inliers.end(); itr++) {
+            x.push_back(cloud->points[*itr].x);
+            y.push_back(cloud->points[*itr].y);
+            z.push_back(cloud->points[*itr].z);
+        }
 
-		float a =  (y2-y1)*(z3-z1) - (z2-z1)*(y3-y1);
-		float b =  (z2-z1)*(x3-x1) - (x2-x1)*(z3-z1);
-		float c =  (x2-x1)*(y3-y1) - (y2-y1)*(x3-x1);
-        float d = -(a*x1 + b*y1 + c*z1);
+		float a =  (y[1]-y[0])*(z[2]-z[0]) - (z[1]-z[0])*(y[2]-y[0]);
+		float b =  (z[1]-z[0])*(x[2]-x[0]) - (x[1]-x[0])*(z[2]-z[0]);
+		float c =  (x[1]-x[0])*(y[2]-y[0]) - (y[1]-y[0])*(x[2]-x[0]);
+        float d = -(a*x[0] + b*y[0] + c*z[0]);
 
 		// Measure distance between every point and fitted line
 		// If distance is smaller than threshold count it as inlier
