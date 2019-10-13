382b13ad720ef04bbd1f0c91bd3592d618905d04
jin <judy_zhujin@hotmail.com>
2019-10-13 15:52:17 -0700

    runs, but result seems incorrect

--- src/environment.cpp
+++ src/environment.cpp
@@ -36,20 +36,20 @@ std::vector<Car> initHighway(bool renderScene,
 void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer,
                ProcessPointClouds<pcl::PointXYZI>* pointProcessorI,
                pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud) {
-  inputCloud = pointProcessorI->FilterCloud(inputCloud, 0.3,
-                                            Eigen::Vector4f(-10, -5, -2, 1),
-                                            Eigen::Vector4f(30, 8, 1, 1));
+  inputCloud = pointProcessorI->FilterCloud(inputCloud, 0.4,
+                                            Eigen::Vector4f(-10, -6, -2, 1),
+                                            Eigen::Vector4f(30, 6, 1, 1));
   std::unordered_set<int> inliers =
-      pointProcessorI->Ransac3D(inputCloud, 25, 3.0);
+      pointProcessorI->Ransac3D(inputCloud, 40, 0.3);
 
   std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr,
             pcl::PointCloud<pcl::PointXYZI>::Ptr>
       segmentCloud = pointProcessorI->SeparateClouds(inliers, inputCloud);
-  // renderPointCloud(viewer, segmentaCloud.first, "obstCloud", Color(1, 0, 0));
-  // renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1,
-  // 0));
+
+  // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+  renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
   std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters =
-      pointProcessorI->Clustering(segmentCloud.first, 0.53, 10, 500);
+      pointProcessorI->Clustering(segmentCloud.first, 0.5, 10, 150);
 
   int clusterId = 0;
   std::vector<Color> colors = {Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1)};
@@ -97,9 +97,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer) {
       pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
 
   // rendering
-  // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-  // renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1,
-  // 0));
+  renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+  renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
 
   int clusterId = 0;
   std::vector<Color> colors = {Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1)};
